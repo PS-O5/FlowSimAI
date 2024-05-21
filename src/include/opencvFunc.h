a369e499c6822f289974cdfbb96e9a9cf7ceed13
Pratik Suryawanshi <77827740+PS-O5@users.noreply.github.com>
2024-05-21 23:13:40 +0200

    Create opencvFunc.h

---
+++ src/include/opencvFunc.h
@@ -0,0 +1,9 @@
+// image_difference.h
+#ifndef IMAGE_DIFFERENCE_H
+#define IMAGE_DIFFERENCE_H
+
+#include <opencv2/opencv.hpp>
+
+cv::Mat computeImageDifference(const cv::Mat& img1, const cv::Mat& img2);
+
+#endif // IMAGE_DIFFERENCE_H
